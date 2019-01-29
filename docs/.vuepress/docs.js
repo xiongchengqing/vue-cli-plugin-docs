bf44d1145d094647222dae4c4eaa74637add4b5e
Xiong,Cheng-Qing <chengqing.xiong@gmail.com>
2019-01-29 18:25:19 +0800

    update, theme refactor to be done

--- docs/.vuepress/docs.js
+++ docs/.vuepress/docs.js
@@ -1,3 +1,11 @@
+const striptags = require('./strip-tags')
+const convert = (str) => {
+    str = str.replace(/(&#x)(\w{4});/gi, function($0) {
+        return String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16));
+    });
+    return str;
+}
+
 
 module.exports = (options, ctx) => {
     return {
@@ -5,6 +13,10 @@ module.exports = (options, ctx) => {
 
         enhanceDevServer(app) {
             console.log('app', app)
+        },
+
+        enhanceAppFiles() {
+
         },
 
         extendMarkdown(md)  {
@@ -18,24 +30,30 @@ module.exports = (options, ctx) => {
                 render(tokens, idx) {
                     // tokens see reflink: https://img.xiongc.com/tokens_from_markdownit.json
                     // idx is the current token index
-                    const m = tokens[idx].info.trim().match(new RegExp(`^${name}\\s+(.*)$`))
+                    var m = tokens[idx].info.trim().match(new RegExp(`^${name}\\s+(.*)$`))
+                    var content = tokens[idx + 1] ? tokens[idx + 1].content : ''
+
+                    // const html = convert(striptags.strip(content, ['script', 'style'])).replace(/(<[^>]*)=""(?=.*>)/g, '$1')
+                    var html = striptags.strip(content, ['script', 'style'])
+                    var description = (m && m.length > 1) ? m[1] : ''
+                    var descriptionHTML = description ? md.render(description).html : ''
 
                     if (tokens[idx].nesting === 1) {
                         // opening tag
                         // md is an instance of markdown-it, and plz ref its API Doc 
                         // https://markdown-it.github.io/markdown-it/#Renderer.render
                         return `
-                                <details>
-                                    <summary> 
-                                        ${md.render(m[1]).html} 
-                                    </summary>
-                                `
+                            <demo-block class="demo-box">
+                                <div class="source" slot="source">${html}</div>
+                                    ${descriptionHTML}
+                                <div class="highlight" slot="highlight">`;
 
                     } else {
                         // closing tag
                         return `
-                                </details>
-                                `
+                                </div>
+                            </demo-block>`;
+
                     }
                 }
             })
--- docs/README.md
+++ docs/README.md
@@ -1,12 +1,79 @@
 
-### Layout 布局
-
-通过基础的 24 分栏，迅速简便地创建布局。
+### Demo Block
 
 ::: demo 这是一个简单的 `Button`, 下面中演示地代码将会自动转化成一个按钮
 ```html
 <template>
-  <button @click="hello">对这个世界多一点善意</button>
+  <el-alert
+    title="成功提示的文案"
+    type="success">
+  </el-alert>
+  <el-alert
+    title="消息提示的文案"
+    type="info">
+  </el-alert>
+  <el-alert
+    title="警告提示的文案"
+    type="warning">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error">
+  </el-alert>
+</template>
+```
+:::
+
+### 自定义关闭按钮
+
+自定义关闭按钮为文字或其他符号。
+
+:::demo 在 Alert 组件中，你可以设置是否可关闭，关闭按钮的文本以及关闭时的回调函数。`closable`属性决定是否可关闭，接受`boolean`，默认为`true`。你可以设置`close-text`属性来代替右侧的关闭图标，注意：`close-text`必须为文本。设置`close`事件来设置关闭时的回调。
+```html
+<template>
+  <el-alert
+    title="不可关闭的 alert"
+    type="success"
+    :closable="false">
+  </el-alert>
+  <el-alert
+    title="自定义 close-text"
+    type="info"
+    close-text="知道了">
+  </el-alert>
+  <el-alert
+    title="设置了回调的 alert"
+    type="warning"
+    @close="hello">
+  </el-alert>
 </template>
 
 <script>
@@ -19,9 +86,4 @@
   }
 </script>
 ```
-:::
-
-
-::: demo 这是一个代码块演示框2
-This is a demo block here
 :::
